6040e8762d4357d52cd93c083daa5939cf713723
benya7 <bitup.games@gmail.com>
2024-07-19 14:46:58 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Challenge() {
   return (
     <div className='min-h-screen bg-[--off-white] flex items-center justify-center'>
       <Turnstile
-        options={{ theme: "light" }}
+        options={{ theme: "light", appearance: "interaction-only"}}
         siteKey={process.env.NEXT_PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY as string}
         onSuccess={() => router.push("/app")}
       />
